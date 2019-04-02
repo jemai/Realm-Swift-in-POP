c024264ec4a02f2156bdc947ff8231d341bebd5c
Abdelhak Jemaii <abdelhak.jemaii@endios.de>
2019-04-02 11:29:55 +0200

    Initial Commit

---
+++ RealmDB-Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmDB-Manager
+//
+//  Created by Abdelhak Jemaii on 02.04.19.
+//  Copyright © 2019 Abdelhak Jemaii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RealmDB-ManagerTests/RealmDB_ManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  RealmDB_ManagerTests.swift
+//  RealmDB-ManagerTests
+//
+//  Created by Abdelhak Jemaii on 02.04.19.
+//  Copyright © 2019 Abdelhak Jemaii. All rights reserved.
+//
+
+import XCTest
+@testable import RealmDB_Manager
+
+class RealmDB_ManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
